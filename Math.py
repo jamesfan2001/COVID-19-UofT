e8bcefec3cfbb838aaca4431e9f197d503513370
Mohaddadnia <62353736+Mohaddadnia@users.noreply.github.com>
2020-04-02 23:02:21 -0400

    Update Math.py

--- Math.py
+++ Math.py
@@ -1,4 +1,13 @@
+import numpy as np
+import pandas as pd
+import csv
+
 #SIR model equations
+
+#Force of infection
+def force_of_infection(percent_pop_infected, transmission_rate=0.05):
+  gamma = transmission_rate * percent_pop_infected
+  return gamma
 
 #dS/dt: (S is the number of people that are susceptible) = susceptible_rate
 
@@ -14,3 +23,23 @@ def infected_rate(a, b, S, I):
 
 def recovered_rate(b, I)
 	return b * I
+############################################
+#Canda's population:
+N = 37660320
+
+data = csv.reader(open('Recovered.csv', 'r'), delimiter=",")
+recovered_date, recovered_value = [], []
+
+for row in data:
+  if row[1] == "Canada":
+    recovered_date.append(row[4])
+    recovered_value.append(row[5])
+
+
+data = csv.reader(open('Infected.csv', 'r'), delimiter=",")
+infected_date, infected_value = [], []
+
+for row in data:
+  if row[1] == "Canada":
+    infected_date.append(row[4])
+    infected_value.append(row[5])
